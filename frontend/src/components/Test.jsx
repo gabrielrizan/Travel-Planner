ec570e70ac6eb87a1e90590dfcb32d4e73425d77
Gabriel <rizangabriel1@gmail.com>
2024-03-28 23:24:17 +0200

    started working on the navbar, currently it has an error will fix later

---
+++ frontend/src/components/Test.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+
+const Test = () => {
+  return (
+    <>
+      <div>Test</div>
+    <FontAwesomeIcon icon={faMagnifyingGlass} />   
+    </>
+  )
+}
+
+export default Test
