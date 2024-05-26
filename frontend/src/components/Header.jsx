780c32b36e103587c32bdc1304ff8c84cab79392
Gabriel <rizangabriel3@gmail.com>
2024-05-26 19:16:50 +0300

    rolled back to old logo, it looks better

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Box, CardMedia, Typography } from "@mui/material";
-import logo2 from "../assets/logo2.webp";
+import sponsor from "../assets/sponsor.png";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 
 const Header = () => {
   return (
-    <Container maxWidth="xl">
+    <Container maxWidth="xl" sx={{ marginBottom: "10px" }}>
       <Box display="flex" alignItems="center" justifyContent="space-between">
         <Box>
           <Typography variant="h3">Sign in, save money</Typography>
           <Typography variant="h5">Save 10% or more at participating properties. Just look for the blue Genius label.</Typography>
         </Box>
-        <CardMedia component="img" alt="Sponsor Image" image={logo2} style={{ maxWidth: "300px", height: "300px" }} />
+        <CardMedia component="img" alt="Sponsor Image" image={sponsor} style={{ maxWidth: "300px", height: "300px" }} />
       </Box>
     </Container>
   );
